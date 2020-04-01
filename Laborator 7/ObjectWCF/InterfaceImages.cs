552f41b1d0a2b9a9e83e282e287c97edf551a6fd
RaresVLD <steam_vlaiduc@yahoo.com>
2020-04-01 13:11:09 +0300

    Laborator 7

---
+++ Laborator 7/ObjectWCF/InterfaceImages.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectWCF
+{
+    [ServiceContract]
+    interface InterfaceImages
+    {
+        [OperationContract]
+        bool insertImage(string Name, string Path, string Description, string Location, string Persons);
+
+        [OperationContract]
+        Dictionary<int, string> getImages();
+
+        [OperationContract]
+        bool deleteImage(int id);
+
+        [OperationContract]
+        Dictionary<string, string> getImageData(int id);
+
+        [OperationContract]
+        bool updateImage(int id, string Name, string Path, string Description, string Location, string Persons);
+    }
+}
